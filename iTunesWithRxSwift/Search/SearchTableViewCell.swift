706e64738fd04fe6babdf036c2c7d6e625bd35a6
knockknock <yangban11111@gmail.com>
2024-08-11 22:41:39 +0900

    [Design] SearchTableViewCell에 stackView 위치 설정  - starLabel, companyLabel, genreLabel

--- iTunesWithRxSwift/Search/SearchTableViewCell.swift
+++ iTunesWithRxSwift/Search/SearchTableViewCell.swift
@@ -15,6 +15,16 @@ class SearchTableViewCell: UITableViewCell {
     lazy var iconImageView = UIImageView()
     let titleLabel = UILabel()
     let downLoadButton = UIButton()
+    let starLabel = UILabel()
+    let companyLabel = UILabel()
+    let genreLabel = UILabel()
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [starLabel, companyLabel, genreLabel])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        return stack
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,7 +45,7 @@ class SearchTableViewCell: UITableViewCell {
 
 extension SearchTableViewCell: BaseProtocol {
     func configureHierarchy() {
-        [iconImageView, titleLabel, downLoadButton].forEach { contentView.addSubview($0) }
+        [iconImageView, titleLabel, downLoadButton, stackView].forEach { contentView.addSubview($0) }
     }
     
     func configureLayout() {
@@ -55,6 +65,18 @@ extension SearchTableViewCell: BaseProtocol {
             make.width.equalTo(60)
             make.height.equalTo(32)
         }
+        starLabel.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).offset(4)
+            make.leading.equalTo(iconImageView)
+            make.height.equalTo(24)
+            make.width.equalTo(contentView.frame.width).multipliedBy(1/3)
+        }
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).offset(4)
+            make.leading.equalTo(iconImageView)
+            make.trailing.equalTo(downLoadButton)
+            make.height.equalTo(24)
+        }
     }
     
     func configureUI() {
@@ -66,7 +88,20 @@ extension SearchTableViewCell: BaseProtocol {
         
         downLoadButton.configuration = .download
 
-
+        starLabel.textAlignment = .left
+        starLabel.text = "★ 4.7"
+        starLabel.textColor = .lightGray
+        starLabel.font = .systemFont(ofSize: 13)
+        
+        companyLabel.text = "kakao Corp."
+        companyLabel.textAlignment = .center
+        companyLabel.textColor = .lightGray
+        companyLabel.font = .systemFont(ofSize: 13)
+        
+        genreLabel.text = "navigation"
+        genreLabel.textAlignment = .right
+        genreLabel.textColor = .lightGray
+        genreLabel.font = .systemFont(ofSize: 13)
     }
     
     
